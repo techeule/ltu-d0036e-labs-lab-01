ec3d4edd0e76fa467861d2743bc17564caf3819c
Wassim Akachi <Wassim@BytesAndCodes.com>
2023-09-05 00:47:30 +0200

    Task 2: Get some insight - enhance output

--- lab1/task2.py
+++ lab1/task2.py
@@ -12,40 +12,52 @@ if __name__ == "__main__":
         "min",
         grades,
         min_value(grades),
-        "It is the smallest value in the list")
+        """
+        It is the smallest value in the list
+        """)
 
     print_pretty(
         "max",
         grades,
         max_value(grades),
-        "It is the largest value in the list")
+        """
+        It is the largest value in the list
+        """)
 
     print_pretty(
         "mean",
         grades,
         mean_value(grades),
-        "It is the average value of all numbers in the list")
+        """
+        It is the average value of all numbers in the list
+        """)
 
     print_pretty(
         "standard deviation",
         grades,
         std_deviation_value(grades),
-        "It is a measure, how the data is spread out around the mean. "
-        "If the number is small, all number are spread tightly around the mean. "
-        "If the number is large, all the number are spread out more and not so in the near of the mean value.")
+        """
+        It is a measure, how the data is spread out around the mean.
+        If the number is small, all number are spread tightly around the mean.
+        If the number is large, all the number are spread out more and not so in the near of the mean value.
+        """)
 
     print_pretty(
         "median",
         grades,
         median_value(grades),
-        "First, the numeric list should be sorted first. "
-        "If the list contains an odd number of entries (numbers), then the middle number is the median. "
-        "If the list contains an even number of entries (numbers), then the two number in the middle build an average, "
-        "that is the median.")
+        """
+        First, the numeric list should be sorted first.
+        If the list contains an odd number of entries (numbers), then the middle number is the median.
+        If the list contains an even number of entries (numbers), then the two number in the middle build an average,
+        that is the median.
+        """)
 
     print_pretty(
         "median absolute deviation",
         grades,
         median_abs_dev_value(grades),
-        "It is the deviation around the median value of the absolute difference values between the median "
-        "and the entries of the given numbers, MAD = median(|Y_i - median(Y)|), where Y the list of the numbers.")
+        """
+        It is the deviation around the median value of the absolute difference values between the median
+        and the entries of the given numbers, MAD = median(|Y_i - median(Y)|), where Y the list of the numbers.
+        """)
