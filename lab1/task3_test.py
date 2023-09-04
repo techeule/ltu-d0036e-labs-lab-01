a94ebbb61b35ec1288ae45653fb36f128c7d4173
Wassim Akachi <Wassim@BytesAndCodes.com>
2023-09-05 00:51:20 +0200

    Task 3: Load and inspect the housing data - make test cases more reliable

--- lab1/task3_test.py
+++ lab1/task3_test.py
@@ -1,15 +1,19 @@
 import unittest
+import os
 
 from lab1 import load_plain_csv, load_dataframe_csv
+
+HERE = os.path.dirname(__file__)
+root_path = os.path.abspath(HERE)
 
 
 class LoadCsvTest(unittest.TestCase):
     def test_load_csv_default_args(self):
-        csv_lines = load_plain_csv("example.csv")
+        csv_lines = load_plain_csv(os.path.join(root_path, "example.csv"))
         self.assertEqual(8, len(csv_lines))
 
     def test_load_csv_as_dataframe(self):
-        csv_dataframe = load_dataframe_csv("example.csv")
+        csv_dataframe = load_dataframe_csv(os.path.join(root_path, "example.csv"))
         describe = csv_dataframe.describe()
         self.assertEqual(10, len(describe.columns))
         self.assertEqual(7, len(csv_dataframe))
